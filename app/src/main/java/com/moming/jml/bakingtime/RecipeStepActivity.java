14bba39cd77fcd5e7e12526dc4a53372235aff11
Moming-Desgin <1192653484@qq.com>
2018-01-13 09:43:11 +0800

    20180113

---
+++ app/src/main/java/com/moming/jml/bakingtime/RecipeStepActivity.java
@@ -0,0 +1,15 @@
+package com.moming.jml.bakingtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.moming.jml.backingtime.R;
+
+public class RecipeStepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_step);
+    }
+}
